5955e55543cd7e25e812507f8e02d8a468a9ef07
Md. Rony <rony15-10527@diu.edu.bd>
2022-02-17 10:33:18 +0600

    script file updated

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-function getInputElement(expense) {
-  const incomeInput = document.getElementById(expense +'-input');
-  const foodInput = document.getElementById(expense +'-input');
-  const rentInput = document.getElementById(expense +'-input');
-  const clothInput = document.getElementById(expense +'-input');
+function getInputValue(expense) {
+  const input = document.getElementById(expense + '-input');
+  const amount = parseFloat(input.value);
+  input.value = '';
+  
+  return amount;
 }
 
-document.getElementById('calculate-btn').addEventListener('click', function () {
-  const incomeValue = parseFloat(incomeInput.value);
-  const food = foodInput.value;
-  const rent = rentInput.value;
-  const cloth = clothInput.value;
-  const foodValue = parseFloat(food);
-  const rentValue = parseFloat(rent);
-  const clothValue = parseFloat(cloth);
+
+function updateTotalExpense(foodValue, rentValue, clothValue) {
   const sum = foodValue + rentValue + clothValue;
-  const total = incomeValue - sum;
- 
   const expenseText = document.getElementById('total-expense');
   const totalExp = expenseText.innerText;
   const totalExpense = parseFloat(totalExp);
   expenseText.innerText = sum;
-  incomeInput.value = '';
-  foodInput.value = '';
-  rentInput.value = '';
-  clothInput.value = '';
-  
+  return sum;
+}
+
+function updateTotalBalance(incomeValue,sum) {
   const balanceText = document.getElementById('balance-amount');
   const balance = balanceText.innerText;
   const totalBalance = parseFloat(balance);
+  const total = incomeValue - sum;
   balanceText.innerText = total;
+}
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+  const incomeInput = getInputValue('income');
+  const foodInput = getInputValue('food');
+  const rentInput = getInputValue('rent');
+  const clothInput = getInputValue('cloth');
+  if (incomeInput >= 0 && foodInput >= 0 && rentInput >= 0 && clothInput >= 0) {
+    const sum = updateTotalExpense(foodInput, rentInput, clothInput);
+    updateTotalBalance(incomeInput, sum);
+  } else {
+    
+  }
 
 });
